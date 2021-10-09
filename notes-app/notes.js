9343f5e1399c3b2e5c453c68e11c5954dced9510
Roy Hevrony <roy.hevrony@gmail.com>
2021-10-09 09:09:14 +0300

    last commit

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -10,18 +10,18 @@ const getNotes = () => {
 const removeNote = (title) => {
     const notes = loadNotes()
     const duplicateNotes = notes.filter((note) => note.title !== title)
-    if (notes.length === duplicateNotes.length){
+    if (notes.length === duplicateNotes.length) {
         console.log(chalk.bgRed.bold("Note with title doesn't exist"))
     } else {
         console.log(chalk.bgGreen.bold("Note removed!"))
         saveNotes(duplicateNotes)
     }
 }
-const addNote = (title, body) =>{
+const addNote = (title, body) => {
     const notes = loadNotes()
-    const duplicateNotes = notes.filter((note)=> note.title === title)
-
-    if (duplicateNotes.length == 0){
+    const duplicateNotes = notes.filter((note) => note.title === title)
+    debugger
+    if (duplicateNotes.length == 0) {
         notes.push({
             title: title,
             body: body
@@ -36,7 +36,7 @@ const addNote = (title, body) =>{
 const readNote = (title) => {
     const notes = loadNotes()
     const noteRead = notes.find((note) => note.title === title)
-    if (noteRead){
+    if (noteRead) {
         console.log(chalk.green(noteRead.title))
         console.log(noteRead.body)
     } else {
@@ -52,10 +52,10 @@ const saveNotes = (notes) => {
 
 const loadNotes = () => {
     try {
-    const dataBuffer = fs.readFileSync('notes.json')
-    const dataJSON = dataBuffer.toString()
-    return JSON.parse(dataJSON)
-    } catch(e){
+        const dataBuffer = fs.readFileSync('notes.json')
+        const dataJSON = dataBuffer.toString()
+        return JSON.parse(dataJSON)
+    } catch (e) {
         return []
     }
 }
